531a0b0fb181261857aa473a9cbd87a5727fb668
Pavel Kononov <woodsleaf@gmail.com>
2019-09-07 15:28:56 +0300

    Update tkinterShpora.py

--- tkinterShpora.py
+++ tkinterShpora.py
@@ -1,4 +1,4 @@
-#### трирешетки подряд означают что данный элемент несовместим с системой вкладок, он должен быть внутри  а не снаружи, как показано в самом начале
+#### Вкладки отключил, так как пока не знаю как совместить их с остальным grid Ошибка(_tkinter.TclError: cannot use geometry manager pack inside . which already has slaves managed by grid)
 from tkinter import *
 #lib dropdowninput
 from tkinter.ttk import Combobox
@@ -59,17 +59,6 @@ window = Tk()
 #Заголовок окна
 window.title("Добро пожаловать в приложение PythonRu")
 window.geometry('400x600')
-#Вкладки
-tab_control = ttk.Notebook(window)
-tab1 = ttk.Frame(tab_control)
-tab2 = ttk.Frame(tab_control)
-tab_control.add(tab1, text='Первая')
-tab_control.add(tab2, text='Вторая')
-lbl1 = Label(tab1, text='Вкладка 1', padx=5, pady=5)
-lbl1.grid(column=0, row=0)
-lbl2 = Label(tab2, text='Вкладка 2', padx=5, pady=5)
-lbl2.grid(column=0, row=0)
-tab_control.pack(expand=1, fill='both')
 #Меню
 menu = Menu(window)
 new_item = Menu(menu) #, tearoff=0
@@ -80,24 +69,38 @@ new_item.add_separator()
 new_item.add_command(label='Изменить')
 menu.add_cascade(label='Файл', menu=new_item)
 window.config(menu=menu)
+#Вкладки
+'''
+tab_control = ttk.Notebook(window)
+tab1 = ttk.Frame(tab_control)
+tab2 = ttk.Frame(tab_control)
+tab_control.add(tab1, text='Первая')
+tab_control.add(tab2, text='Вторая')
+lbl1 = Label(tab1, text='Вкладка 1', padx=5, pady=5)
+lbl1.grid(column=0, row=0)
+lbl2 = Label(tab2, text='Вкладка 2', padx=5, pady=5)
+lbl2.grid(column=0, row=0)
+tab_control.pack(expand=1, fill='both')
+'''
+
 #dropdownlistAndInput
 combo = Combobox(window, width=10)
 combo['values'] = (0,1, 2, 3, 4, 5, "Текст")
 combo.current(0)  # установите вариант по умолчанию
 #combo.get()
-###combo.grid(column=0, row=0)
+combo.grid(column=0, row=0)
 #button
 btn = Button(window, command=clickedBtn, text="Клик", bg="white", fg="black")
-###btn.grid(column=1, row=0)
+btn.grid(column=1, row=0)
 #button2
 btnClear = Button(window, command=clickedBtnClear, text="ClearTxtScroll", bg="white", fg="gray")
-###btnClear.grid(column=3, row=0)
+btnClear.grid(column=3, row=0)
 #btnInfo
 btnInfo = Button(window, command=clickedBtnInfo, text="Info", bg="white", fg="gray")
-###btnInfo.grid(column=4, row=0)
+btnInfo.grid(column=4, row=0)
 #btnImport
 btnImport = Button(window, command=clickedBtnImport, text="Import", bg="white", fg="gray")
-###btnImport.grid(column=4, row=0)
+btnImport.grid(column=4, row=0)
 #checkbox
 # можно так
 ##chk_state = IntVar()
@@ -107,29 +110,29 @@ btnImport = Button(window, command=clickedBtnImport, text="Import", bg="white", 
 chk_state = BooleanVar()
 chk_state.set(True)  # задайте проверку состояния чекбокса
 chk = Checkbutton(window, text='Выбрать', var=chk_state)
-###chk.grid(column=2, row=0)
+chk.grid(column=2, row=0)
 #radio
 selected = IntVar()
 rad1 = Radiobutton(window, text='Первый', value=1, variable=selected)
 rad2 = Radiobutton(window, text='Второй', value=2, variable=selected)
 rad3 = Radiobutton(window, text='Третий', value=3, variable=selected)
-###rad1.grid(column=0, row=1)
-###rad2.grid(column=1, row=1)
-###rad3.grid(column=2, row=1)
+rad1.grid(column=0, row=1)
+rad2.grid(column=1, row=1)
+rad3.grid(column=2, row=1)
 #input
 txt = Entry(window, width=10) #, state='disabled'
-###txt.grid(column=0, row=2)
+txt.grid(column=0, row=2)
 txt.focus()
 #text
 lbl = Label(window, text="Привет", font=("Arial Bold", 18))
-###lbl.grid(column=0, row=3)
+lbl.grid(column=0, row=3)
 #text2
 lbl2 = Label(window, text="Number", font=("Arial Bold", 18))
-###lbl2.grid(column=0, row=4)
+lbl2.grid(column=0, row=4)
 #scrolled window
 txtscroll = scrolledtext.ScrolledText(window, width=10, height=10)
 txtscroll.insert(INSERT, 'Текстовое поле')
-###txtscroll.grid(column=0, row=5)
+txtscroll.grid(column=0, row=5)
 
 #spin поле ввода с возможностью выбора значения из интервала, с стрелочками
 var = IntVar()
@@ -138,7 +141,7 @@ spin = Spinbox(window, from_=0, to=100, width=5, textvariable=var)
 #Выбор из определенных значений
 #spin = Spinbox(window, values=(3, 8, 11), width=5)
 #Значение по умолчанию
-###spin.grid(column=0, row=6)
+spin.grid(column=0, row=6)
 
 #Стилизованный прогресс бар
 #Настройка стиля
@@ -148,7 +151,7 @@ style.configure("black.Horizontal.TProgressbar", background='black')
 #Последним параметром указан стиль
 bar = Progressbar(window, length=100, style='black.Horizontal.TProgressbar')
 bar['value'] = 70 #в %
-###bar.grid(column=0, row=7)
+bar.grid(column=0, row=7)
 
 # must be end
 window.mainloop()
