8e227845215125e9c8ca6e568d20e313c051c11c
Pavel Kononov <woodsleaf@gmail.com>
2019-09-08 00:16:11 +0300

    Update tkinterShpora.py

--- tkinterShpora.py
+++ tkinterShpora.py
@@ -17,6 +17,18 @@ from tkinter import Menu
 from os import path
 from tkinter import filedialog
 
+'''
+Переменная SCRIPT_FOLDER полезна при задании путей к файлам, которые всегда находятся в одном и том же месте
+ относительно скрипта: файлы настроек, сохранения, токены, коды приложений,
+  и т. д. Таким образом я гарантирую, что программа считает файл из правильного
+   места вне зависимости от того, откуда она была запущена (например, из другой папки или через демона).
+'''
+if getattr(sys, 'frozen', False):
+	# frozen
+	SCRIPT_FOLDER = path.dirname(sys.executable)
+else:
+	SCRIPT_FOLDER = path.dirname(path.realpath(__file__))
+
 #Загрузка информации  пути файла(ов) в переменную, и отображение ее в поле txtscroll
 def clickedBtnImport():
     #С указанием типа файла
@@ -31,6 +43,23 @@ def clickedBtnImport():
     #Вывод в поле
     txtscroll.delete(1.0, END)
     txtscroll.insert(INSERT, file)
+
+#Чтение контента файла и подсчет слов, вывод в поле
+def counter():
+    output.delete("0.0","end")
+    filename = filedialog.askopenfilename()
+    with open(filename) as file:    #открываем через менеджер контекста, filename определим позже
+        text = file.read()    #считываем содержимое
+    text = text.replace("\n", " ")
+    text = text.replace(",", "").replace(".", "").replace("?", "").replace("!", "").replace("—", "")
+    text = text.lower()    #убираем верхний регистр
+    words = text.split()    #создаем в список, где каждый элемент — слово
+    output.insert("end","Amount of words: %d\n" % len(words))
+    output.insert("end","Amount of nonrepeatable words: %d\n" % len(words))
+
+#Запись контента gjkz output в файл
+def export():
+    file = ''
 
 #from input to text
 def clickedBtn():
@@ -71,13 +100,13 @@ menu.add_cascade(label='Файл', menu=new_item)
 window.config(menu=menu)
 #Вкладки
 '''
-tab_control = ttk.Notebook(window)
-tab1 = ttk.Frame(tab_control)
-tab2 = ttk.Frame(tab_control)
+tab_control = ttk.Notebook(window) # определяет зону для грида window
+tab1 = ttk.Frame(tab_control) # определяет зону для грида tab1
+tab2 = ttk.Frame(tab_control) # определяет зону для грида tab2
 tab_control.add(tab1, text='Первая')
 tab_control.add(tab2, text='Вторая')
-lbl1 = Label(tab1, text='Вкладка 1', padx=5, pady=5)
-lbl1.grid(column=0, row=0)
+lbl1 = Label(tab1, text='Вкладка 1', padx=5, pady=5)  #Первый аргумент зона грида
+lbl1.grid(column=0, row=0)  #Место/порядок в гриде column-колонка, row, строка, если происходит пересечение то описанный ниже элемент ханимает место ранее описанного и тот не отображается вовсе.
 lbl2 = Label(tab2, text='Вкладка 2', padx=5, pady=5)
 lbl2.grid(column=0, row=0)
 tab_control.pack(expand=1, fill='both')
@@ -98,9 +127,16 @@ btnClear.grid(column=3, row=0)
 #btnInfo
 btnInfo = Button(window, command=clickedBtnInfo, text="Info", bg="white", fg="gray")
 btnInfo.grid(column=4, row=0)
-#btnImport
+#btnImport pathAndNameFile
 btnImport = Button(window, command=clickedBtnImport, text="Import", bg="white", fg="gray")
 btnImport.grid(column=4, row=0)
+#btnImport contentOtherFile
+btnContentFile = Button(window, text="Import file...", command=counter)    #создаём кнопку
+btnContentFile.grid(column=5, row=0, pady=4)
+#btnExport contentOtherFile
+btnExportToFile = Button(window, text="Import file...", command=export)    #создаём кнопку
+btnExportToFile.grid(column=6, row=0, pady=4)
+
 #checkbox
 # можно так
 ##chk_state = IntVar()
@@ -152,6 +188,9 @@ style.configure("black.Horizontal.TProgressbar", background='black')
 bar = Progressbar(window, length=100, style='black.Horizontal.TProgressbar')
 bar['value'] = 70 #в %
 bar.grid(column=0, row=7)
+
+output = Text(window, width=45, height=3)    #создаём поле вывода
+output.grid(row=8, columnspan=6) #columnspan занимает 8 колонок ?
 
 # must be end
 window.mainloop()
